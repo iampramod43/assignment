07320d4840d3afae08c3160530e305a1e9721a98
Pramod Ukkali <pramod.ukkali@instavans.com>
2021-03-03 21:55:00 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -6,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'assignment';
+  headerChange = false;
+
+  @HostListener('mousewheel', ['$event']) onMouseWheelChrome(event) {
+      // console.log(event)
+        this.headerChange = true;
+    }
+  
+    @HostListener('DOMMouseScroll', ['$event']) onMouseWheelFirefox(event: any) {
+        this.headerChange = true;
+    }
+  
+    @HostListener('onmousewheel', ['$event']) onMouseWheelIE(event: any) {
+        this.headerChange = true;
+    }
+    @HostListener('document:scroll') onmouseover() {
+      console.log('over')
+    }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,20 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { GlobalConstants } from '../constants';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  headerChange = GlobalConstants.headerChange;
 
+  
   constructor() { }
 
   ngOnInit(): void {
-    this.click();
+    // window.addEventListener('scroll', () => {
+    //   if (window.screenY > 100) {
+    //     handleShow(true);
+    //   } else handleShow(false);
+    // })
   }
 
-  click() {
-    console.log('clicked');
+  // @HostListener("document:scroll")
+  // scrollFunction() {
+  //   console.log('hello')
+  //   if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+  //     this.headerChange = true;
+  //   } else {
+  //     this.headerChange = false;
+  //   }
+  // }
+  // @HostListener('mousewheel', ['$event']) onMouseWheelChrome(event: any) {
+  //   console.log(event)
+  //   this.headerChange = true;
+  // }
+
+  // @HostListener('DOMMouseScroll', ['$event']) onMouseWheelFirefox(event: any) {
+  //     this.headerChange = true;
+  // }
+
+  // @HostListener('onmousewheel', ['$event']) onMouseWheelIE(event: any) {
+  //     this.headerChange = true;
+  // }
+
+  @HostListener('click') doSomething() {
+    alert('it work');
   }
 
 }
